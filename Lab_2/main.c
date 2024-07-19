89aa14f02cf4077f07206e5c147b7b3e56d0391f
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-07-19 08:27:33 -0600

    sa

---
+++ Lab_2/main.c
@@ -0,0 +1,29 @@
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED_PIN PB5  // El pin PB5 corresponde al pin digital 13 en el Arduino Nano
+
+void setup() {
+	// Configurar el pin del LED como salida
+	DDRB |= (1 << LED_PIN);
+}
+
+void loop() {
+	// Encender el LED
+	PORTB |= (1 << LED_PIN);
+	_delay_ms(500);
+	// Apagar el LED
+	PORTB &= ~(1 << LED_PIN);
+	_delay_ms(500);
+}
+
+int main(void) {
+	setup();
+	while (1) {
+		loop();
+	}
+	return 0;
+}
