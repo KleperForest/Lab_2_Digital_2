56b374dcfd4e0ca99067c09217f36d6390c03a1c
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-07-19 15:17:20 -0600

    DRF

--- Lab_2/main.c
+++ Lab_2/main.c
@@ -1,29 +1,30 @@
-// Ejemplo de led parpadeante PB5 Arduino Nano atmega 328p
 #define F_CPU 16000000UL
-
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_PIN PB5  // El pin PB5 corresponde al pin digital 13 en el Arduino Nano
+#define LED_PIN PB5
+#define DELAY_TIME 1000 // 1000 ms = 1 segundo
 
 void setup() {
-	// Configurar el pin del LED como salida
-	DDRB |= (1 << LED_PIN);
+    // Configura el pin LED_PIN como salida
+    DDRB |= (1 << LED_PIN);
 }
 
 void loop() {
-	// Encender el LED
-	PORTB |= (1 << LED_PIN);
-	_delay_ms(500);
-	// Apagar el LED
-	PORTB &= ~(1 << LED_PIN);
-	_delay_ms(500);
+    // Enciende el LED
+    PORTB |= (1 << LED_PIN);
+    _delay_ms(DELAY_TIME);
+    
+    // Apaga el LED
+    PORTB &= ~(1 << LED_PIN);
+    _delay_ms(DELAY_TIME);
 }
 
 int main(void) {
-	setup();
-	while (1) {
-		loop();
-	}
-	return 0;
+    setup();
+    while (1) {
+        loop();
+    }
+    
+    return 0;
 }
